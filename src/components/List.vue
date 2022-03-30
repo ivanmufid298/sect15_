9582af4f8ff5de8f79fa13d058961fb6bd917d8d
ivan mufid <ivanmohammad2355@gmail.com>
2022-03-30 11:41:44 +0700

    Section 15

---
+++ src/components/List.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'Home',
+  computed: {
+    listBerita () {
+      return this.$store.state.berita.list
+    }
+  },
+  methods: {
+    fetchBerita () {
+      this.$store.dispatch('berita/fetchlist')
+    }
+  },
+  mounted () {
+    this.fetchBerita()
+  }
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    list: []
   },
   mutations: {
+    setList (state, param) {
+      state.list = param
+    }
   },
   actions: {
+    fetchList (store) {
+      axios.get('https://newsapi.org/v2/everything?q=bitcoin&apiKey=99da639dc1ce4117912c24440c9ecf6d').then((response) => {
+        store.commit('setList', response.data.results)
+      })
+    }
   },
   modules: {
   }
